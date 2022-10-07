522096d7b60b37162d1f39ad2c59bb5efc611172
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-10-07 07:56:33 -0600

    Getting Started

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const { PORT, MONGODB_URI } = process.env;
+const express = require("express");
+const app = express();
+	
+	///////////////////////////////
+	// ROUTES
+	////////////////////////////////
+	
+	app.get("/", (req, res) => {
+	    res.send("hello world");
+	});
+
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
